dec7db871d47f4d64c81d39eadd7b9753c05a958
Nazeer Ahmed <nazeerafridi690@gmail.com>
2024-07-19 15:21:39 +0500

    update

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,12 +3,12 @@
 
 
 const config = {
-  apiKey: "AIzaSyA9XKIwM0LzcD0PzMi2IeLxv15iJ_4Zhzo",
-  authDomain: "arise-96645.firebaseapp.com",
-  databaseURL: "https://arise-96645.firebaseio.com",
-  projectId: "arise-96645",
-  storageBucket: "arise-96645.appspot.com",
-  messagingSenderId: "453896234258"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 export default config;
